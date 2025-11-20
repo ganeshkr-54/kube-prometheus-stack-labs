7be87cc797199a56c266f768b0494c08e2213301
ganeshkr-54 <ganeshkumarreddy54@gmail.com>
2025-11-20 11:19:23 -0500

    Clean initial commit (secrets removed)

---
+++ playground-sample/main.tf
@@ -0,0 +1,104 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.53.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~>4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+  features {}
+  skip_provider_registration = true
+}
+
+data "azurerm_resource_group" "example" {
+  name = var.resource_group_name
+}
+
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  resource_group_name  = data.azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.example.id
+  }
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "example_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.example_ssh.private_key_pem
+  filename = "${path.module}/id_rsa"
+  file_permission = "0600"
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
+  size                = "Standard_B2s"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+}
+
+output "private_key_pem" {
+  value     = tls_private_key.example_ssh.private_key_pem
+  sensitive = true
+}
---
+++ playground-sample/variables.tf
@@ -0,0 +1,12 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "westus"
+  description = "Location of the resource group."
+}
+
+
+variable "resource_group_name" {
+  type        = string
+  default     = "kml_rg_main-c93f1f34803a4e46"
+  description = "Resource group name in your Azure subscription."
+}
